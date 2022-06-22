96498538bcb6aed952b8dcd16bc5ca511b6c62e6
NiconicoLab <lanesra.xxx@gmail.com>
2022-06-22 22:58:42 +0900

    add sample program using val

---
+++ val.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+path1=/hoge/hoge/file.txt
+path2=/hoge/hoge/file.csv
+path3=/hoge/hoge/file.xls
+path4=/hoge/hoge/file.pdf
+path5=/hoge/hoge/file.cpp
+
+echo ${path1}
+echo ${path2}
+echo ${path3}
+echo ${path4}
+echo ${path5}
+
+echo "========================="
+../c/bash/arg ${path1} ${path2} ${path3} ${path4} ${path5}
