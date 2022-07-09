42420a0db0e87e62129096e1903079e776b512c6
NiconicoLab <lanesra.xxx@gmail.com>
2022-07-09 14:11:42 +0900

    update called.sh for adding conditional branch

--- called.sh
+++ called.sh
@@ -6,6 +6,15 @@ if [ $# -ne 3 ]; then
   echo "Usage\$$0 requires 3 arguments" 1>&2
 else
   echo "$1 $2 $3"
+  if [ $1 = temp1 ]; then
+    echo "\$1 is match (string : temp1)"
+  fi
+  if [ $2 = temp2 ]; then
+    echo "\$2 is match (string : temp2)"
+  fi
+  if [ $3 = temp3 ]; then
+    echo "\$3 is match (string : temp3)"
+  fi
 fi
 
 echo "end   === $0 ==="
